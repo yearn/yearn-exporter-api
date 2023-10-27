e32761ec900d1d3ba90946042ee9b81ae3315879
0xBasic <0xBasic@yearn.finance>
2023-10-27 15:30:40 -0400

    add: base, yeth

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from grafana import get_for, get_partners_for
 app = Flask(__name__)
 CORS(app)
 
-NETWORKS = ['eth', 'ftm', 'opt', 'arb']
+NETWORKS = ['eth', 'ftm', 'opti', 'arbi', 'base']
 
 UNIT_USD = 'USD'
 
